f379c9ff037321d6c682d1c7c921051696151066
Oglo12 <ogloooglooagla@gmail.com>
2023-05-30 18:39:34 -0500

    Cleaned up theme.js.

--- scripts/theme.js
+++ scripts/theme.js
@@ -2,18 +2,22 @@ const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
 
 function loadTheme() {
 	var cssId = 'myCss';
+
 	if (!document.getElementById(cssId)) {
 		var head = document.getElementsByTagName('head')[0];
 		var link = document.createElement('link');
+
 		link.id = cssId;
 		link.rel = 'stylesheet';
 		link.type = 'text/css';
+		link.media = 'all';
+
     		if (darkModeQuery.matches) {
 			link.href = 'themes/dark.css';
 		} else {
 			link.href = 'themes/light.css';
 		}
-		link.media = 'all';
+
 		head.appendChild(link);
 	}
 }
