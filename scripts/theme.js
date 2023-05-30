5e2e247397098ac696a060088d7f93e476fd92ff
Oglo12 <ogloooglooagla@gmail.com>
2023-05-30 18:36:59 -0500

    Light and dark mode!

---
+++ scripts/theme.js
@@ -0,0 +1,22 @@
+const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+function loadTheme() {
+	var cssId = 'myCss';
+	if (!document.getElementById(cssId)) {
+		var head = document.getElementsByTagName('head')[0];
+		var link = document.createElement('link');
+		link.id = cssId;
+		link.rel = 'stylesheet';
+		link.type = 'text/css';
+    		if (darkModeQuery.matches) {
+			link.href = 'themes/dark.css';
+		} else {
+			link.href = 'themes/light.css';
+		}
+		link.media = 'all';
+		head.appendChild(link);
+	}
+}
+
+loadTheme();
+darkModeQuery.addEventListener('change', loadTheme);
